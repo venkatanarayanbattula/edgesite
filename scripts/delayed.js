c5594008e341022331b07dc176685e435c79939d
venkatanarayanbattula <94977341+venkatanarayanbattula@users.noreply.github.com>
2025-02-26 15:51:57 +0530

    Update delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,7 @@
-// add delayed functionality here
+// eslint-disable-next-line import/no-cycle
+import { sampleRUM } from './aem.js';
+
+// Core Web Vitals RUM collection
+sampleRUM('cwv');
+
+// add more delayed functionality here
